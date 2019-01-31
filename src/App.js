af0b6ae81e42bef81c8cabfab7984d9131cbfe9e
MikkoKarvonen <mkarvonen34@gmail.com>
2019-01-31 16:52:18 +0200

    Add data to work with

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+/*let Parser = require('rss-parser');
+let parser = new Parser();
+(async () => {
+  let feed = await parser.parseURL("https://cors-anywhere.herokuapp.com/"+'http://feeds.soundcloud.com/users/soundcloud:users:152277214/sounds.rss');
+  feed.items.forEach(item => {
+    console.log(item)
+  });
+  console.log(feed);
+ 
+})();*/
+
+import values from './tempData/data.json'
+console.log(values)
+values.items.forEach(item => {
+  console.log(item)
+});
+
+let randomClass= [
+  'random1', 'random2'
+]
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="App episodes">
+        {values.items.map(function(item, i){
+          return (
+            <div key={i} className="episode">
+              <img src={item.itunes.image} className="coverImage"/>
+              <h2>{item.title}</h2>
+              <iframe width="100%" height="20" scrolling="no" frameborder="no" allow="autoplay" src={"https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/"+item.guid.split('tracks/')[1]+"&color=%23ff5500&inverse=true&auto_play=false&show_user=true"}></iframe>
+              <p>{item.content}</p>
+              <p>{item.pubDate}</p>
+            </div>
+          )
+        })}
       </div>
     );
   }
