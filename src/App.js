eb95cc57ab073d1b3d4f39ab14cfbef6e81e1a13
MikkoKarvonen <mkarvonen34@gmail.com>
2019-01-31 17:26:37 +0200

    Add header

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Header from './components/Header.js';
 import './App.css';
 
 /*let Parser = require('rss-parser');
@@ -18,25 +19,24 @@ values.items.forEach(item => {
   console.log(item)
 });
 
-let randomClass= [
-  'random1', 'random2'
-]
-
 class App extends Component {
   render() {
     return (
-      <div className="App episodes">
-        {values.items.map(function(item, i){
-          return (
-            <div key={i} className="episode">
-              <img src={item.itunes.image} className="coverImage"/>
-              <h2>{item.title}</h2>
-              <iframe width="100%" height="20" scrolling="no" frameborder="no" allow="autoplay" src={"https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/"+item.guid.split('tracks/')[1]+"&color=%23ff5500&inverse=true&auto_play=false&show_user=true"}></iframe>
-              <p>{item.content}</p>
-              <p>{item.pubDate}</p>
-            </div>
-          )
-        })}
+      <div>
+        <Header title={values.title}/>
+        <div className="App episodes">
+          {values.items.map(function(item, i){
+            return (
+              <div key={i} className="episode">
+                <img src={item.itunes.image} className="coverImage"/>
+                <h2>{item.title}</h2>
+                <iframe width="100%" height="20" scrolling="no" frameborder="no" allow="autoplay" src={"https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/"+item.guid.split('tracks/')[1]+"&color=%23ff5500&inverse=true&auto_play=false&show_user=true"}></iframe>
+                <p>{item.content}</p>
+                <p>{item.pubDate}</p>
+              </div>
+            )
+          })}
+        </div>
       </div>
     );
   }
